093f9ebcca74f2757e77960ea177cf6f3054f4ed
Andrew Chung <andrewtc4@proton.me>
2026-01-23 22:04:19 -0500

    emacs_editing: Add send_sequence()
    This allows me to use Emacs-inspired key sequences to achieve various effects

--- keyboards/drop/alt/keymaps/chunga2020/emacs_editing.c
+++ keyboards/drop/alt/keymaps/chunga2020/emacs_editing.c
@@ -20,3 +20,32 @@ bool process_motion(uint16_t keycode, keyrecord_t *record,
     }
     return true;
 }
+
+bool send_sequence(uint16_t keycode, keyrecord_t *record,
+                   layer_state_t accepted_layer_state, uint8_t required_mods,
+                   uint16_t *sequence, uint8_t num_keys)
+{
+    uint8_t mod_state = get_mods();
+
+    if (layer_state_is(accepted_layer_state)) {
+        if (record->event.pressed) {
+            if (mod_state & required_mods) {
+                del_mods(required_mods);
+
+
+                /* Tapping the first item of the sequence before the loop allows
+                 * the loop to be written with the inter-key delay first.  This
+                 * allows the loop to not have any ending lag. */
+                tap_code16(sequence[0]);
+                for (int i = 1; i < num_keys; i++) {
+                    wait_ms(50); /* might need to adjust this interval */
+                    tap_code16(sequence[i]);
+                }
+
+                set_mods(mod_state);
+                return false;
+            }
+        }
+    }
+    return true;
+}
--- keyboards/drop/alt/keymaps/chunga2020/emacs_editing.h
+++ keyboards/drop/alt/keymaps/chunga2020/emacs_editing.h
@@ -20,4 +20,21 @@ bool process_motion(uint16_t keycode, keyrecord_t *record,
                     layer_state_t accepted_layer_state, uint8_t required_mods,
                     uint16_t to_send);
 
+/*
+ * Send a sequence of keypresses from a single physical keypress.
+ *
+ * Parameters:
+ * keycode: the keycode, as for process_record_*
+ * record: the key event record, as for process_record_*
+ * accepted_layer_state: the layer state that must be active for this sequence
+ * to trigger
+ * required_mods: the modifier(s) that must be active to trigger this sequence
+ * sequence: the key sequence to send.  This is a list of keycodes that will be
+ * sent one after another.
+ * num_keys: the number of keys in the sequence, for loop termination
+ */
+bool send_sequence(uint16_t keycode, keyrecord_t *record,
+                   layer_state_t accepted_layer_state, uint8_t required_mods,
+                   uint16_t *sequence, uint8_t num_keys);
+
 #endif
